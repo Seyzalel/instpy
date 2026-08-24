cd807645f8178bac2c49133a6df7c884f65b8659
Seyzalel Menabel <127889263+Seyzalel@users.noreply.github.com>
2026-08-24 12:11:33 -0300

    Update SESSION_ID for Instagram API configuration

--- app.py
+++ app.py
@@ -99,7 +99,7 @@ def bot_firewall():
 # ==========================================
 # CONFIGURAÇÃO DA INSTAGRAPI (OTIMIZADA)
 # ==========================================
-SESSION_ID = "53793198529%3Apu70XULiAZnebN%3A29%3AAYgtYwrY8DR6FJgR_KRrpxchj5i6Xf9AHOi26aqwcw"
+SESSION_ID = "53793198529%3AkgZrvZqmBbxoKh%3A2%3AAYjjhK_4P5YdXqyLYjIfpRexDEFdRGhM2ZdNG-r_Yw"
 PROXY_URL = "http://user-spbdjmclc2-continent-eu:lS8QnaqotlM9i3Y+g3@gate.decodo.com:10001"
 
 insta_client_singleton = None
